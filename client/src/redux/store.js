04376bec4a708575058298ddc1647c05b99c3cd8
ASSOUNE Mike <massoune@nexity.fr>
2019-06-24 10:42:43 +0200

    c

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -4,13 +4,18 @@ import logger from 'redux-logger'
 
 import reducer from 'redux/reducers'
 
+const middlewares = [
+  thunk,
+]
+
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
+
 const store = createStore(
   reducer,
   compose(
-    applyMiddleware(
-      thunk,
-      logger
-    ),
+    applyMiddleware(...middlewares),
     window.devToolsExtension ? window.devToolsExtension() : f => f
   )
 )
