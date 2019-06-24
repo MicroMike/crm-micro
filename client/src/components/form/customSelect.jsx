18fc187622fb2e3539ab4149af00e639be563d4f
ASSOUNE Mike <massoune@nexity.fr>
2019-06-24 17:16:14 +0200

    c

--- client/src/components/form/customSelect.jsx
+++ client/src/components/form/customSelect.jsx
@@ -3,10 +3,9 @@ import React from 'react'
 export default ({ input, values }) => (
   <div className="formGroup">
     <select {...input} id={input.name}>
-      <options></options>
       {
         values.map((v, i) => (
-          <options key={i} value={v}>{v}</options>
+          <option key={i} value={v}>{v}</option>
         ))
       }
     </select>
--- client/src/models/users.js
+++ client/src/models/users.js
@@ -1,5 +1,17 @@
+const listYear = () => {
+  const date = new Date()
+  const dates = []
+  for (let i = 0; i < 100; i++) {
+    dates.push(date.getFullYear() - i)
+  }
+  return dates
+}
+
 module.exports = {
   firstname: String,
   lastname: String,
   gender: { mongoDB: String, type: 'radio', values: ['Homme', 'Femme'] },
+  day: String,
+  month: { mongoDB: String, type: 'select', values: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'] },
+  year: { mongoDB: String, type: 'select', values: listYear() },
 }
